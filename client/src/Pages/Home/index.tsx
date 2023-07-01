57a4058b157720648e18b10d6e076cb2f2d7d480
Peter Osborne <peterosborne248@gmail.com>
2023-07-01 15:14:28 +1000

    map not working

--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+import { useMemo } from "react";
 import SearchBar from '../../Components/SearchBar/index'
 import { ButtonGroup } from 'react-bootstrap';
 
@@ -6,23 +7,30 @@ import './style.scss';
 
 const Mapback = () => {
     return (
-        <div className="background">
-            {
-                <p> This is the map!</p>
-            }
-        </div>
+        <>
+            <div className="background">
+                {
+                    <div className='map-component-container'>
+                        <div>yoooo</div>
+                        <GoogleMap zoom={10} center={{ lat: 44, lng: -80 }} mapContainerClassName='map-container'></GoogleMap>
+                    </div>
+                }
+            </div>
+        </>
     );
 };
 
 
 
 export default function Home() {
+    const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!, });
+    if (!isLoaded) return <div>loading...</div>
 
     return (
         <div className='home-container'>
             <p>This will be signin button</p>
             <div className='map-container'>
-                <Mapback/>
+                <Mapback />
             </div>
         </div>
     )
