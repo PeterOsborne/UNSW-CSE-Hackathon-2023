bff4cea0a8dad63e3e8af510d86f5dccaea58f9d
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2023-07-01 20:46:59 +1000

    started listspotwindow

---
+++ client/src/Components/ListSpotWindow/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './style.scss';
+
+/* 
+This window is where sellers will put their spots up for renting
+*/
+
+interface Props {
+
+}
+
+const ListSpotWindow = (props: Props) => {
+    <div className="bg-white border-4 rounded-lg">
+
+      <div className="flex flex-col">
+        <div className="flex">Lease a parking spot</div>
+      </div>
+
+      <div className="grid-cols-2">
+        <div className="">Parking Type</div>
+        <div>driveway</div>
+        <div>Spot Availability</div>
+        <div>Spot Price</div>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="flex">Post parking spot!</div>
+      </div>
+    </div>
+};
+
+export default ListSpotWindow;
