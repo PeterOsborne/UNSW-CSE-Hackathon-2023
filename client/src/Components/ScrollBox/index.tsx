a28b686dc4d8c8f17e3d807257e2c44573200218
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2023-07-01 13:18:39 +1000

    worked on scrollbar

--- client/src/Components/ScrollBox/index.tsx
+++ client/src/Components/ScrollBox/index.tsx
@@ -1,27 +1,54 @@
 import './index.scss';
 import React, { useState } from 'react';
 
-interface Props {
-
+interface Place {
+  placeDistance: number;
+  placePriceRate: number;
+  placeName: string;
 }
 
 const ScrollBox = () => {
-    const [parkSpots, setParkSpots] = useState([
-        { id: 1, title: '10 Brown St' },
-        { id: 2, title: '5 Mayfair Ave' },
-        { id: 3, title: '20 Green Rd' }
-    ]);
+  const [items, setItems] = useState<Place[]>([
+    { placeDistance: 8, placePriceRate: 3, placeName: 'place 1' },
+    { placeDistance: 6, placePriceRate: 2, placeName: 'place 2' },
+    { placeDistance: 12, placePriceRate: 1, placeName: 'place 3' }
+  ]);
 
-    //if shit do shit
+  const addItem = () => {
+    const newItem = prompt('Enter an item:');
+    if (newItem) {
+      setItems([...items, { placeDistance: 0, placePriceRate: 0, placeName: newItem }]);
+    }
+  };
 
-    return (
-        <div>
-            <div className="overflow-y-scroll">
-                <p>this is my scrollbar</p>
-            </div> 
-        </div>
-    )
+  const removeItem = (index: number) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
 
-}
+  return (
+    <div>
+      <div className="overflow-y-scroll">
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              <div>
+                <strong>placeName:</strong> {item.placeName}
+              </div>
+              <div>
+                <strong>placeDistance:</strong> {item.placeDistance}
+              </div>
+              <div>
+                <strong>placePriceRate:</strong> {item.placePriceRate}
+              </div>
+              <button onClick={() => removeItem(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default ScrollBox;
