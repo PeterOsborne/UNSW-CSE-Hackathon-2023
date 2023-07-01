f85411a0e9f49388b194ec95391e90412068152e
Peter Osborne <peterosborne248@gmail.com>
2023-07-01 12:18:26 +1000

    Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,17 @@ import logo from './logo.svg';
 import { createBrowserRouter, BrowserRouter, Routes, Route, Link, NavLink, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import './App.css';
 
+import Layout from "./Components/Layout/index"
+import Home from "./Pages/Home/index"
+
 function App() {
   return (
     <div className="App">
-
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
