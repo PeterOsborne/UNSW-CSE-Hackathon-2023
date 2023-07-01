3361e52955aba373aba91a3ded08dc53e98b794f
Jesse Herdan <z5480568@ad.unsw.edu.au>
2023-07-02 02:29:55 +1000

    Added create place functionality

--- client/src/Components/ListSpotWindow/index.tsx
+++ client/src/Components/ListSpotWindow/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './index.scss';
 import ParkingTypeDropDown from '../ParkingTypeDropDown';
+// import { getData, setData } from '../../Backend/dataStore'
+import { createPlace } from '../../Backend/places' 
 
 /* 
 This window is where sellers will put their spots up for renting
@@ -8,7 +10,8 @@ This window is where sellers will put their spots up for renting
 
 interface marktype {
     lat: number,
-    lng: number
+    lng: number,
+    type: number,
 }
 interface Place {
     placeDistance: number;
@@ -21,18 +24,85 @@ interface Props {
     markers: marktype[];
 }
 
-const ListSpotWindow = (props: Props) => {
-    let [disabled, setDisabled] = useState(true);
-    const [buttonColor, setButtonColor] = useState('grey');
-    const handleButtonClick = () => {
+const UNSW: marktype = {
+    lat: -33.91668815669858, 
+    lng: 151.23118166932008,
+    type: 0,
+}
 
-        setButtonColor('blue');
-        setDisabled(false);
-    };
-    function handleClick() {
-        console.log("CLICKED");
+const ListSpotWindow = (props: Props) => {
+
+    //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+    function calcCrow(lat1: number, lon1: number, lat2: number, lon2: number): number {
+        var R = 6371; // km
+        var dLat = toRad(lat2-lat1);
+        var dLon = toRad(lon2-lon1);
+        var lat1 = toRad(lat1);
+        var lat2 = toRad(lat2);
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c;
+        return d;
     }
 
+    // Converts numeric degrees to radians
+    function toRad(Value: number): number {
+        return Value * Math.PI / 180;
+    }
+
+    let [disabled, setDisabled] = useState(true);
+    const [buttonColor, setButtonColor] = useState('grey');
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [availStart, setAvailStart] = useState('');
+    const [availEnd, setAvailEnd] = useState('');
+
+    function handleNameChange(event: any) {
+        setName(event.target.value);
+    }
+    function handlePriceChange(event: any) {
+        setPrice(event.target.value);
+    }
+
+    function handleAvaliabilityStartChange(event: any) {
+        setAvailStart(event.target.value);
+    }
+    function handleAvaliabilityEndChange(event: any) {
+        setAvailEnd(event.target.value);
+    }
+
+  
+    function handleClick() {
+        const currentMarker = props.markers[numOfMarkers - 1];
+        const distance = Math.round(calcCrow(currentMarker.lat, currentMarker.lng, UNSW.lat, UNSW.lng) * 10) / 10;
+        const avail = availStart + ' - ' + availEnd;
+        
+
+        let newPlace: Place = {
+            placeDistance: distance,
+            placePriceRate: parseInt(price),
+            placeName: name,
+            placeAvailability: avail,
+            marker: currentMarker,
+        }
+        createPlace(newPlace);
+    }
+
+    let numOfMarkers = props.markers.length;
+    if (
+        props.markers[numOfMarkers - 1].type === 1
+        && buttonColor === 'grey'
+        && name !== ''
+        && price !== ''
+        && availStart !== ''
+        && availEnd !== ''
+    ) {
+        setButtonColor('blue');
+        setDisabled(false);
+    }
 
     return (
         <div>
@@ -47,10 +117,10 @@ const ListSpotWindow = (props: Props) => {
                     <div className="p-2 font-medium">Spot Availability</div>
                     <div className="p-2 flex flex-col">
                         <div>
-                            <input className="w-3/12 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start_avail" type="text" placeholder="00:00"></input> - <input className="w-3/12 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start_avail" type="text" placeholder="24:00"></input>
+                            <input onChange={handleAvaliabilityStartChange} value={availStart} className="w-3/12 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start_avail" type="text" placeholder="00:00"></input> - <input className="w-3/12 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="end_avail" type="text" placeholder="24:00" onChange={handleAvaliabilityEndChange} value={availEnd}></input>
                         </div>
                         <div className="flex justify-center p-3">
-                            <button onClick={handleButtonClick} className="flex text-sm w-8/12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <button className="flex text-sm w-8/12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 + Add Availability
                             </button>
                         </div>
@@ -58,13 +128,13 @@ const ListSpotWindow = (props: Props) => {
                     <div className="p-2 font-medium">Spot Price</div>
                     <div className="p-2">
                         <div>
-                            $ <input className="w-3/12 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start_avail" type="text" placeholder="1.00"></input>/hour
+                            $ <input className="w-3/12 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" type="text" placeholder="1.00" value={price} onChange={handlePriceChange}></input>/hour
                         </div>
                     </div>
-                    <div className="p-2 font-medium">Spot Address</div>
+                    <div className="p-2 font-medium">Spot Name</div>
                     <div className="p-2">
                         <div>
-                            <input className="w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start_avail" type="text" placeholder="12 Walnut St, NSW 2222"></input>
+                            <input className="w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="12 Walnut St, NSW 2222" onChange={handleNameChange} value={name}></input>
                         </div>
                     </div>
                 </div>
