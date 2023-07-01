da2ba3ba233b2605e83790a391e4de1177cf39da
Samuel Katz <samuelkatz2245@gmail.com>
2023-07-01 13:26:03 +1000

    things

--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './style.scss';
 
 export default function Home() {
 
     return (
         <div className='home-container'>
+            <p>This will be signin button</p>
             <div className='map-container'>
+                <p> This is the map!</p>
             </div>
         </div>
     )
