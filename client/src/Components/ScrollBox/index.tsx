ba23c7b4d3076715a3a9ff1e1c9ba5e61c481459
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2023-07-01 12:49:15 +1000

    resolved 3 errors in scrollbar

--- client/src/Components/ScrollBox/index.tsx
+++ client/src/Components/ScrollBox/index.tsx
@@ -1,10 +1,16 @@
 import './index.scss';
+import React, { useState } from 'react';
 
 interface Props {
 
 }
 
 const ScrollBox = () => {
+    const [parkSpots, setParkSpots] = useState([
+        { id: 1, title: '10 Brown St' },
+        { id: 2, title: '5 Mayfair Ave' },
+        { id: 3, title: '20 Green Rd' }
+    ]);
 
     //if shit do shit
 
