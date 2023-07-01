7473a7224643b82d6e3814988dfc3a9dcbc20284
Samuel Katz <samuelkatz2245@gmail.com>
2023-07-02 00:07:19 +1000

    bgs

--- client/src/Components/LookingToBox/index.tsx
+++ client/src/Components/LookingToBox/index.tsx
@@ -6,36 +6,41 @@ import classNames from 'classnames';
 interface PropsLookingToBox {
     option: number //if 0 then rent, if 1 then offer
 }
-
+interface marktype {
+    lat: number,
+    lng: number
+}
 interface Place {
     placeDistance: number;
     placePriceRate: number;
     placeName: string;
     placeAvailability: string;
+    marker: marktype;
 }
 
 export default function LookingToBox(props: PropsLookingToBox) {
 
 
     const [items, setItems] = useState<Place[]>([
-        { placeDistance: 8, placePriceRate: 3, placeName: '10 Norton St, Kingsford NSW 2032', placeAvailability: '09:00 - 16:00' },
-        { placeDistance: 6, placePriceRate: 2, placeName: '20 Milford St, Kingsford NSW 2032', placeAvailability: '09:00 - 16:00' },
-        { placeDistance: 12, placePriceRate: 1, placeName: '23 Rolfe St, Rosebury NSW 2018', placeAvailability: '09:00 - 16:00' },
-        { placeDistance: 12, placePriceRate: 1, placeName: '15 Duke St, Kengiston NSW 2033', placeAvailability: '09:00 - 16:00' }
+        // { placeDistance: 8, placePriceRate: 3, placeName: '10 Norton St, Kingsford NSW 2032', placeAvailability: '09:00 - 16:00' },
+        // { placeDistance: 6, placePriceRate: 2, placeName: '20 Milford St, Kingsford NSW 2032', placeAvailability: '09:00 - 16:00' },
+        // { placeDistance: 12, placePriceRate: 1, placeName: '23 Rolfe St, Rosebury NSW 2018', placeAvailability: '09:00 - 16:00' },
+        // { placeDistance: 12, placePriceRate: 1, placeName: '15 Duke St, Kengiston NSW 2033', placeAvailability: '09:00 - 16:00' }
     ]);
 
     const [selected, setSelected] = useState(items[0]);
 
     if (props.option == 0) {
         return (
-            <>
+            <div className="">
                 <div className={classNames("rentSpot")}>
-                    <ScrollBox items={items} onSelect={(selected: Place, index: number) => {
+                    <ScrollBox onSelect={(selected: Place, index: number) => {
                         setSelected(selected);
-                    }}></ScrollBox>
+
+                    }} items={items}></ScrollBox>
                 </div>
 
-            </>
+            </div>
         );
     }
     return (
