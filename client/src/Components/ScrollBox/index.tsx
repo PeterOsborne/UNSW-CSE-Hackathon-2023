345c08f40e756c536cb75bdca6321a44033c916e
Samuel Katz <samuelkatz2245@gmail.com>
2023-07-02 11:30:11 +1000

    asd

--- client/src/Components/ScrollBox/index.tsx
+++ client/src/Components/ScrollBox/index.tsx
@@ -23,11 +23,14 @@ interface Props {
 
 
 const ScrollBox = (props: Props) => {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(-1);
 
   useEffect(() => {
 
-    props.onSelect(props.items[selected], selected)
+    if (selected >= 0) {
+      props.onSelect(props.items[selected], selected)
+    }
+
   }, [selected])
 
   const [buttonColor, setButtonColor] = useState('grey');
@@ -47,19 +50,21 @@ const ScrollBox = (props: Props) => {
 
   return (
     <div className="scrollbar">
-          <div className="flex flex-col p-1 shadow pt-2">
-              <button style={{ backgroundColor: buttonColor }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
-                  Book this spot!
-              </button>
-          </div>
+      <div className="flex flex-col p-1 shadow pt-2">
+        {selected != -1 &&
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+            Book this spot!
+          </button>}
+      </div>
 
       <div className="">
         <ul className=""> {/* overflow-y-scroll */}
 
           {props.items.map((item, index) => (
             <li key={index} onClick={() => {
-
               setSelected(index)
+
+
             }}>
               <div className={classNames("p-1", "box-item-cont")} >
                 <div className={classNames("pl-6 pt-2 pb-2 pr-6", "box-item", "drop-shadow-xl", index === selected && "selected")}>
